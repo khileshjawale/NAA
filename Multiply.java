ddb999f1719e2b204f753bc64b251f379d59bf36
Bhargav Joshi <134037039+Bhargav080803@users.noreply.github.com>
2025-08-22 16:33:40 +0530

    commit - Multiply Two Integers
    returns int value multiplied with two integers

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+public class Multiply{
+    public static void main(String[] args) {
+        int a=10,b=10;
+        System.out.println(multiply(a,b));
+    }
+
+    public static int multiply(int a,int b)
+    {
+        return a*b;
+    }
+
+}
